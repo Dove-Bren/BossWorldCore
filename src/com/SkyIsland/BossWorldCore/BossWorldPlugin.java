53439f68a27ce5e6baa0065fe750a2ff0a93ab62
William Fong <trigition@gmail.com>
2014-09-28 01:04:19 -0600

    Added basic debug print messages to check if plugin actually loads

--- src/com/SkyIsland/BossWorldCore/BossWorldPlugin.java
+++ src/com/SkyIsland/BossWorldCore/BossWorldPlugin.java
@@ -5,15 +5,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class BossWorldPlugin extends JavaPlugin {
 	
 	public void onEnable() {
+		this.getLogger().info("LOOOL This plugin is enaabled");
 		
 	}
 	
 	public void onDisable() {
-		
+		this.getLogger().info("NOOOO This plugin is now disaabled");
 	}
 	
 	public void onLoad() {
-		
+		this.getLogger().info("YOLO This plugin is loaaded");
 	}
 	
 	
