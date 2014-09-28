fad082f6eda29313de1f524d8d10bbad3e2c27bd
Skyler <skymanzanares@hotmail.com>
2014-09-27 22:59:59 -0600

    First commit that creates packages and plugin class

---
+++ src/com/SkyIsland/BossWorld/BossWorldPlugin.java
@@ -0,0 +1,21 @@
+package com.SkyIsland.BossWorld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BossWorldPlugin extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public void onLoad() {
+		
+	}
+	
+	
+	
+}
